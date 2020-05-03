7173dd6dd1f84cccdcb41edac968546224ab8813
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-03 22:47:33 +0900

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :name,  presence: true
+  validates :email, presence: true
 end
