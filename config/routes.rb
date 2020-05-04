38c752d998a25e3dc4e60c3478dc7c5cf96f345f
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-04 13:13:12 +0900

    Execute rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # User
   mount_devise_token_auth_for "User", at: "auth"
   # Article
-  namespace :api, format: 'json' do
+  namespace :api, format: "json" do
     namespace :v1 do
       resources :articles
     end
