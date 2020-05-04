64bc19a3b5a96b0c8bc8f76885130bfb1d123fd4
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-04 12:20:48 +0900

    Add article's routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # User
   mount_devise_token_auth_for "User", at: "auth"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Article
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :articles
+    end
+  end
 end
