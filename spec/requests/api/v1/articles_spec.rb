32d027254d34e7d29d2a680109d46cdce2f35a55
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-05 17:01:20 +0900

    Add request spec for show action

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Articles", type: :request do
       it "任意の article の値が取得できる" do
         subject
         res = JSON.parse(response.body)
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
 
         expect(res["title"]).to eq article.title
         expect(res["content"]).to eq article.content
