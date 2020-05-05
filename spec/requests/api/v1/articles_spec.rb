d951f28a28476e609b2ffdbe1a2177a0746bddde
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-05 10:47:49 +0900

    Create request spec about index action in articles controller

---
+++ spec/requests/api/v1/articles_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Articles", type: :request do
+  describe "GET /articles" do
+    subject { get(api_v1_articles_path) }
+    before do
+      create_list(:article, 3)
+    end
+    it "articleの一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["id", "title", "content", "created_at", "updated_at", "comments", "article_likes"]
+      expect(response).to have_http_status(200)
+    end
+  end
+end
