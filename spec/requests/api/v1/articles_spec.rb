7968b9409bb54eea5fbff948b0c8db3e3ebffb22
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-05 14:19:56 +0900

    Correct articles_spec

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -4,16 +4,18 @@ RSpec.describe "Articles", type: :request do
   describe "GET api/v1/articles" do
     subject { get(api_v1_articles_path) }
 
-    before do
-      create_list(:article, 3)
-    end
+    context "DB上にarticleレコードが存在する場合" do
+      before do
+        create_list(:article, 3)
+      end
 
-    it "articleの一覧が取得できる" do
-      subject
-      res = JSON.parse(response.body)
-      expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id", "title", "content", "created_at", "updated_at", "user", "article_likes"]
-      expect(response).to have_http_status(:ok)
+      it "articleの一覧が取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.length).to eq 3
+        expect(res[0].keys).to eq ["id", "title", "content", "created_at", "updated_at", "number_of_likes", "user"]
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
