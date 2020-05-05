3f7bd5a49962b8a43841bf920773f07fd877afb0
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-05 14:18:26 +0900

    Rename method about article_likes count

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,8 +1,8 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :created_at, :updated_at, :like_count
+  attributes :id, :title, :content, :created_at, :updated_at, :number_of_likes
   belongs_to :user
 
-  def like_count
+  def number_of_likes
     object.article_likes.count
   end
 end
