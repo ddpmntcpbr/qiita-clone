daeee699a23de38f5a1deb7e9f055946a12884f9
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-09 16:36:27 +0900

    Add homes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  root "homes#index"
+
   mount_devise_token_auth_for "User", at: "auth"
   namespace :api, format: "json" do
     namespace :v1 do
