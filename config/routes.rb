1bef44d041d8a345dfb471c14882d014d6649e93
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-10 12:23:52 +0900

    Redefine routing to registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "homes#index"
 
-  mount_devise_token_auth_for "User", at: "auth"
+  mount_devise_token_auth_for "User", at: "api/v1/auth", controllers: {
+    registrations: 'api/v1/auth/registrations'
+  }
+
   namespace :api, format: "json" do
     namespace :v1 do
       resources :articles
