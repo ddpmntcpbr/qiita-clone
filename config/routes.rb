6b35e9bdc800b40dcb790787951053c8443c94c4
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-10 12:29:19 +0900

    Execute rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "homes#index"
 
   mount_devise_token_auth_for "User", at: "api/v1/auth", controllers: {
-    registrations: 'api/v1/auth/registrations'
+    registrations: "api/v1/auth/registrations",
   }
 
   namespace :api, format: "json" do
