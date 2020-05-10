58abd3bc860fede3bf7012c756620b41d71d2702
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-10 14:52:28 +0900

    Change rails version in migrate files

--- db/migrate/20200503093055_create_article_likes.rb
+++ db/migrate/20200503093055_create_article_likes.rb
@@ -1,4 +1,4 @@
-class CreateArticleLikes < ActiveRecord::Migration[6.0]
+class CreateArticleLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :article_likes do |t|
       t.references :user, null: false, foreign_key: true
