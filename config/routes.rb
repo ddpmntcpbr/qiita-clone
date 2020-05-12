b10d8f16d8f6f98307a3ba92154ba70d3674054f
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-12 22:03:56 +0900

    Correct route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root "homes#index"
 
-  mount_devise_token_auth_for "User", at: "api/v1/auth", controllers: {
-    registrations: "api/v1/auth/registrations",
-  }
-
   namespace :api, format: "json" do
     namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth", controllers: {
+        registrations: "api/v1/auth/registrations",
+      }
       resources :articles
     end
   end
