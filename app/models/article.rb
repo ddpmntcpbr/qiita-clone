51fabed873982f36de054aa3971b752b65541b55
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-14 21:18:45 +0900

    AddStatusToArticles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,6 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true
+
+  enum status: { draft: false, published: true }
 end
