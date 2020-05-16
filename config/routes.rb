68d24a1bc64869feb61dc5a6de7dcebe3eb92190
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-16 15:55:39 +0900

    Correct routing order

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
-      resources :articles
-      namespace :articles do
+      namespace "articles" do
         resources :drafts, only: [:index, :show]
       end
+      resources :articles
     end
   end
 end
