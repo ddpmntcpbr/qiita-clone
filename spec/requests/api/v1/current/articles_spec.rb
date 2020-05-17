3873211f85edc15601b0675614a167097ac2bfb9
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-05-17 19:17:33 +0900

    Correct current articles spec

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -7,17 +7,20 @@ RSpec.describe "Api::V1::Current::Articles", type: :request do
     let(:headers) { current_user.create_new_auth_token }
     let(:current_user) { create(:user) }
 
+    let!(:article1) { create(:article, user: current_user, created_at: 10.days.ago, updated_at: 1.day.ago) }
+    let!(:article2) { create(:article, user: current_user, created_at: 3.days.ago, updated_at: 2.days.ago) }
+
     context "カレントユーザーが公開状態の articles を保存している場合" do
       before do
-        create_list(:article, 3, user: current_user)
-        create_list(:article, 2, :save_draft, user: current_user)
+        create(:article, :save_draft, user: current_user)
+        create(:article)
       end
 
       it "公開状態の article の一覧が取得できる" do
         subject
-        expect(Article.count).to eq 5
         res = JSON.parse(response.body)
-        expect(res.length).to eq 3
+        expect(res.length).to eq 2
+        expect(res.map {|d| d["id"] }).to eq [article2.id, article1.id]
         expect(res[0].keys).to eq ["id", "title", "content", "created_at", "updated_at", "likes_count", "user"]
         expect(response).to have_http_status(:ok)
       end
